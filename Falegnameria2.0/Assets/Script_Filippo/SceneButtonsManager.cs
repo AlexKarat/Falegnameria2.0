98bd3aaa986066d8b5bece9d598b95c5c30182aa
FiliBenzi <filippo.benzi.14-2@accademiadigitaleliguria.it>
2025-11-07 11:46:15 +0100

    ButtonWINeG.OVER

---
+++ Falegnameria2.0/Assets/Script_Filippo/SceneButtonsManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButtonsManager : MonoBehaviour
+{
+    [Header("Nomi delle Scene")]
+    public string menuSceneName = "MainMenu";   // scena del menù (puoi cambiarlo in seguito)
+    public string hubSceneName = "Hub";         // scena dell’hub (puoi cambiarlo in seguito)
+    public string miniGiocoSceneName = "MiniGiocoLegna"; // 👈 aggiunto per restart
+
+    // Torna al menù principale
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(menuSceneName);
+    }
+
+    // Riavvia il minigioco
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(miniGiocoSceneName);
+    }
+
+    // Torna all'hub
+    public void GoToHub()
+    {
+        SceneManager.LoadScene(hubSceneName);
+    }
+
+    // Esce dal gioco
+    public void QuitGame()
+    {
+        Debug.Log("Uscita dal gioco");
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // così funziona anche in Play Mode
+#endif
+    }
+}
